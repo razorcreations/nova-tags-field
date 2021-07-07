1ea9fc55362093a8c2c67404f30aacd993ec825e
James <jamesharleyweb@gmail.com>
2021-07-07 14:04:33 +0100

    fix field help text

--- resources/js/components/Nova/FormField.vue
+++ resources/js/components/Nova/FormField.vue
@@ -1,5 +1,5 @@
 <template>
-    <default-field :field="field" :errors="errors" :show-help-text="showHelpText">
+    <default-field :field="field" :errors="errors" :show-help-text="field.helpText">
         <template slot="field">
             <component
                 :is="component"
